4278a0394f634d74f25d3500d859cf7df6850f3c
supragyan panda <supragyan.panda@symphonyteleca.com>
2015-03-06 10:09:08 +0530

    Did development of DTO Enhancement. 1.Created JQGridJSON page for displaying Product table data in JSON format. 2.Created JSONService.cs for selecting data from Product table. 3.Created and implemented code for SaveJQGridJSON method.

---
+++ root/programs/C#/Samples/WebApp_sample/ProjectX_sample/App_Code/IJSONService.cs
@@ -0,0 +1,33 @@
+﻿//**********************************************************************************
+//* フレームワーク・テスト画面
+//**********************************************************************************
+
+//**********************************************************************************
+//* クラス名        ：JSONService
+//* クラス日本語名  ：JSONService
+//*
+//* 作成日時        ：－
+//* 作成者          ：－
+//* 更新履歴        ：－
+//*
+//*  日時        更新者            内容
+//*  ----------  ----------------  -------------------------------------------------
+//*  2015/03/05  Supragyan         Created IJSONService interface for invoking Product table data.
+//**********************************************************************************
+using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IJSONService" in both code and config file together.
+[ServiceContract(SessionMode = SessionMode.Allowed)]
+public interface IJSONService
+{
+    [OperationContract]
+    [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetProductData")]
+    void GetProductData();
+}
---
+++ root/programs/C#/Samples/WebApp_sample/ProjectX_sample/App_Code/JSONService.cs
@@ -0,0 +1,53 @@
+﻿//**********************************************************************************
+//* フレームワーク・テスト画面
+//**********************************************************************************
+
+//**********************************************************************************
+//* クラス名        ：JSONService
+//* クラス日本語名  ：JSONService
+//*
+//* 作成日時        ：－
+//* 作成者          ：－
+//* 更新履歴        ：－
+//*
+//*  日時        更新者            内容
+//*  ----------  ----------------  -------------------------------------------------
+//*  2015/03/05  Supragyan         Created JSONService for invoking Product table data.
+//**********************************************************************************
+
+//System
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//Touryo
+using Touryo.Infrastructure.Business.Common;
+using Touryo.Infrastructure.Framework.Common;
+using Touryo.Infrastructure.Public.Db;
+using Touryo.Infrastructure.Business.Util;
+using Touryo.Infrastructure.Business.Business;
+using Touryo.Infrastructure.Public.Dto;
+
+public class JSONService : IJSONService
+{
+    /// <summary>
+    /// GetProductData method for fetching product table data.
+    /// </summary>
+    public void GetProductData()
+    {
+        HttpContext.Current.Session["DAP"] = "SQL";
+        HttpContext.Current.Session["DBMS"] = DbEnum.DBMSType.SQLServer;
+
+        ProductsTableAdapter productTableAdapter = new ProductsTableAdapter();
+        DataTable productTableData = productTableAdapter.SelectMethod(0, 30);
+        int count = productTableAdapter.SelectCountMethod();
+
+        //Calling SavejqGridJson
+        DTTable dtTable = new DTTable("Product");
+        dtTable.SavejqGridJson(productTableData, count);
+    }
+}
