a718324153b4108186f838330451f859320130a4
daisukenishino <daisuke.nishino.zg@hitachi-solutions.com>
2017-08-21 13:48:49 +0900

    Now, I am fixing #234

---
+++ root/programs/C#/NuGet/_NuGetPack.bat
@@ -0,0 +1,7 @@
+..\..\nuget.exe pack Public.nuspec -OutputDirectory out
+..\..\nuget.exe pack Framework.nuspec -OutputDirectory out
+..\..\nuget.exe pack DamManagedOdp.nuspec -OutputDirectory out
+..\..\nuget.exe pack DamPstGrS.nuspec -OutputDirectory out
+..\..\nuget.exe pack DamMySQL.nuspec -OutputDirectory out
+
+pause
