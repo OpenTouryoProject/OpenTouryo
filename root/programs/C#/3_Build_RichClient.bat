ed90b78311e5b4de1b9e26acf302eaac667f7d0e
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2017-09-22 19:28:33 +0900

    Checked the build, and adjusted.

--- root/programs/C#/3_Build_RichClient.bat
+++ root/programs/C#/3_Build_RichClient.bat
@@ -22,11 +22,14 @@ md "Frameworks\Infrastructure\Temp"
 md "Frameworks\Infrastructure\Build"
 
 rem --------------------------------------------------
-rem Output xcopy after you build the batch Infrastructure(RichClientCustomControl)
+rem Output xcopy after you build the batch Infrastructure(RichClientComponent)
 rem --------------------------------------------------
-%BUILDFILEPATH% %COMMANDLINE% "Frameworks\Infrastructure\RichClientCustomControl.sln"
+%BUILDFILEPATH% %COMMANDLINE% "Frameworks\Infrastructure\RichClientComponent.sln"
 
+xcopy /E /Y "Frameworks\Infrastructure\Framework\RichClient\bin\%BUILD_CONFIG%" "Frameworks\Infrastructure\Temp\%BUILD_CONFIG%\"
+xcopy /E /Y "Frameworks\Infrastructure\Business\RichClient\bin\%BUILD_CONFIG%" "Frameworks\Infrastructure\Temp\%BUILD_CONFIG%\"
 xcopy /E /Y "Frameworks\Infrastructure\CustomControl\RichClient\bin\%BUILD_CONFIG%" "Frameworks\Infrastructure\Temp\%BUILD_CONFIG%\"
+
 xcopy /E /Y "Frameworks\Infrastructure\Temp\%BUILD_CONFIG%" "Frameworks\Infrastructure\Build\"
 
 pause
