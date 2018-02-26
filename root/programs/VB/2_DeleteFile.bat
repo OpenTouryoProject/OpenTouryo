c4308f14c8195b55d5a9947fa16366206b3af681
daisuke nishino <nishi_74322014@ksj.biglobe.ne.jp>
2018-02-26 09:27:30 +0900

    Update 2_DeleteFile.bat

--- root/programs/VB/2_DeleteFile.bat
+++ root/programs/VB/2_DeleteFile.bat
@@ -1,13 +1,13 @@
 @echo off
 
 @echo --------------------------------------------------
-@echo Delete the suo *.user *.tmp *.bak *.log files.
+@echo Delete the *.suo *.user *.tmp *.log *.bak *.skrold.
 @echo --------------------------------------------------
 
 del /f /s /a- *.suo *.user *.tmp *.log *.bak *.skrold
 
 @echo --------------------------------------------------
-@echo Deleted the suo *.user *.tmp *.bak *.log files.
+@echo Deleted the *.suo *.user *.tmp *.log *.bak *.skrold.
 @echo --------------------------------------------------
 
 pause
