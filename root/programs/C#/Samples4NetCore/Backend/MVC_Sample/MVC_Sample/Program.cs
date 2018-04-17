45a1c7fc8c62853e519bc7f440a9659a4d0d8807
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-04-17 09:58:03 +0900

    Now, I am corresponding to #235.

--- root/programs/C#/Samples4NetCore/Backend/MVC_Sample/MVC_Sample/Program.cs
+++ root/programs/C#/Samples4NetCore/Backend/MVC_Sample/MVC_Sample/Program.cs
@@ -1,25 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//**********************************************************************************
+//* テンプレート
+//**********************************************************************************
+
+// サンプル中のテンプレートなので、必要に応じて使用して下さい。
+
+//**********************************************************************************
+//* クラス名        ：Program
+//* クラス日本語名  ：Program
+//*
+//* 作成日時        ：－
+//* 作成者          ：－
+//* 更新履歴        ：－
+//*
+//*  日時        更新者            内容
+//*  ----------  ----------------  -------------------------------------------------
+//*  20xx/xx/xx  ＸＸ ＸＸ         ＸＸＸＸ
+//**********************************************************************************
+
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace MVC_Sample
 {
+    /// <summary>Program</summary>
     public class Program
     {
+        /// <summary>
+        /// Main（エントリポイント）</summary>
+        /// <param name="args">コマンドライン引数</param>
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            // BuildWebHostが返すIWebHostをRunする。
+            // 呼び出し元スレッドは終了までブロックされる。
+            Program.BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        /// <summary>BuildWebHost</summary>
+        /// <param name="args">コマンドライン引数</param>
+        /// <returns>IWebHost</returns>
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            // WebHost経由で、IWebHost, IWebHostBuilderにアクセスする。
+
+            return WebHost.CreateDefaultBuilder(args) //  IWebHostBuilderを取得する。
+                .UseStartup<Startup>() // IWebHostBuilder.UseStartup<TStartup> メソッドにStartupクラスを指定。
+                .Build(); // IWebHostBuilder.Build メソッドでIWebHostクラスインスタンスを返す。
+        }
     }
 }
