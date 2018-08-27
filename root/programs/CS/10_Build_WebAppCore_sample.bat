cb0ae785358d6e169d85e4e59c83901210c850fb
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-08-27 11:06:56 +0900

    tweak.

--- root/programs/CS/10_Build_WebAppCore_sample.bat
+++ root/programs/CS/10_Build_WebAppCore_sample.bat
@@ -18,9 +18,12 @@ call %CURRENT_DIR%z_Common.bat
 rem --------------------------------------------------
 rem Batch build of MVC_Sample.
 rem --------------------------------------------------
+
 cd "Samples4NetCore\Backend\MVC_Sample\MVC_Sample"
+if exist "node_modules" rd /s /q "node_modules"
 call npm install
 cd "..\..\..\..\"
+
 dotnet restore "Samples4NetCore\Backend\MVC_Sample\MVC_Sample.sln"
 dotnet msbuild %COMMANDLINE% "Samples4NetCore\Backend\MVC_Sample\MVC_Sample.sln"
 
