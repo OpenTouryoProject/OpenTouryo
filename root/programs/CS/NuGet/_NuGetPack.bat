f5de3096f4aeb75a416deb9e0d7e328ea7789bd5
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-09-14 20:44:28 +0900

    For some reason, the NuGet registration bat files did not work, so they were fixed.

--- root/programs/CS/NuGet/_NuGetPack.bat
+++ root/programs/CS/NuGet/_NuGetPack.bat
@@ -6,21 +6,18 @@ xcopy /E /Y "..\Frameworks\Infrastructure\Build_net46" "in\net46"
 xcopy /E /Y "..\Frameworks\Infrastructure\Build_net47" "in\net47"
 xcopy /E /Y "..\Frameworks\Infrastructure\Build_netstd20\netstandard2.0" "in\netstandard2.0"
 
-@rem PrimaryPackageの生成
-..\..\nuget.exe pack Public.nuspec -OutputDirectory out\pp
-..\..\nuget.exe pack Framework.nuspec -OutputDirectory out\pp
-..\..\nuget.exe pack Framework.RichClient.nuspec -OutputDirectory out\pp
-..\..\nuget.exe pack DamManagedOdp.nuspec -OutputDirectory out\pp
-..\..\nuget.exe pack DamPstGrS.nuspec -OutputDirectory out\pp
-..\..\nuget.exe pack DamMySQL.nuspec -OutputDirectory out\pp
+"..\..\nuget.exe" pack Public.nuspec -OutputDirectory "out\pp"
+"..\..\nuget.exe" pack Framework.nuspec -OutputDirectory "out\pp"
+"..\..\nuget.exe" pack Framework.RichClient.nuspec -OutputDirectory "out\pp"
+"..\..\nuget.exe" pack DamManagedOdp.nuspec -OutputDirectory "out\pp"
+"..\..\nuget.exe" pack DamPstGrS.nuspec -OutputDirectory "out\pp"
+"..\..\nuget.exe" pack DamMySQL.nuspec -OutputDirectory "out\pp"
 
-@rem PrimaryPackageとSymbolPackageの生成
-..\..\nuget.exe pack Symbol_Public.nuspec -OutputDirectory out\sp -Symbols
-..\..\nuget.exe pack Symbol_Framework.nuspec -OutputDirectory out\sp -Symbols
-..\..\nuget.exe pack Symbol_Framework.RichClient.nuspec -OutputDirectory out\sp -Symbols
-..\..\nuget.exe pack Symbol_DamManagedOdp.nuspec -OutputDirectory out\sp -Symbols
-..\..\nuget.exe pack Symbol_DamPstGrS.nuspec -OutputDirectory out\sp -Symbols
-..\..\nuget.exe pack Symbol_DamMySQL.nuspec -OutputDirectory out\sp -Symbols
-
+"..\..\nuget.exe" pack Symbol_Public.nuspec -OutputDirectory "out\sp" -Symbols
+"..\..\nuget.exe" pack Symbol_Framework.nuspec -OutputDirectory "out\sp" -Symbols
+"..\..\nuget.exe" pack Symbol_Framework.RichClient.nuspec -OutputDirectory "out\sp" -Symbols
+"..\..\nuget.exe" pack Symbol_DamManagedOdp.nuspec -OutputDirectory "out\sp" -Symbols
+"..\..\nuget.exe" pack Symbol_DamPstGrS.nuspec -OutputDirectory "out\sp" -Symbols
+"..\..\nuget.exe" pack Symbol_DamMySQL.nuspec -OutputDirectory "out\sp" -Symbols
 
 pause
--- root/programs/CS/NuGet/out/sp/_NuGetPush.bat
+++ root/programs/CS/NuGet/out/sp/_NuGetPush.bat
@@ -1,11 +1,9 @@
 ﻿setlocal
 @echo off
 
-@rem ApiKeyを登録
-..\..\..\..\nuget.exe SetApiKey [ApiKey]
+"..\..\..\..\nuget.exe" SetApiKey [ApiKey]
 
-@rem symbolsource.orgにSymbolPackageを登録
-..\..\..\..\nuget.exe push Touryo.Infrastructure.Public.*.symbols.nupkg -source https://nuget.smbsrc.net/
-..\..\..\..\nuget.exe push Touryo.Infrastructure.Framework.*.symbols.nupkg -source https://nuget.smbsrc.net/
+"..\..\..\..\nuget.exe" push Touryo.Infrastructure.Public.*.symbols.nupkg -source https://nuget.smbsrc.net/
+"..\..\..\..\nuget.exe" push Touryo.Infrastructure.Framework.*.symbols.nupkg -source https://nuget.smbsrc.net/
 
 pause
