ae3a3d6d38b7c85d959b64d5d7471a47e85040d4
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-09-14 19:46:27 +0900

    Additional fixes for v02-30 release.

--- root/programs/CS/Samples4NetCore/Backend/MVC_Sample/MVC_Sample/Controllers/HomeController.cs
+++ root/programs/CS/Samples4NetCore/Backend/MVC_Sample/MVC_Sample/Controllers/HomeController.cs
@@ -19,16 +19,19 @@
 
 using MVC_Sample.Models.ViewModels;
 
+using System;
+using System.Linq;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Security.Claims;
 
-using Microsoft.AspNetCore.Http;
+using System.Web;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using Newtonsoft.Json;
@@ -36,14 +39,40 @@ using Newtonsoft.Json.Linq;
 
 using Touryo.Infrastructure.Business.Presentation;
 using Touryo.Infrastructure.Business.Util;
+using Touryo.Infrastructure.Framework.Authentication;
 using Touryo.Infrastructure.Framework.Util;
+using Touryo.Infrastructure.Public.Security;
 
 namespace MVC_Sample.Controllers
 {
     [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class HomeController : MyBaseMVControllerCore
     {
-        #region 認証不要
+        /// <summary>Nonce</summary>
+        public string Nonce
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(HttpContext.Session.GetString("nonce")))
+                {
+                    HttpContext.Session.SetString("nonce", GetPassword.Base64UrlSecret(10));
+                }
+                return HttpContext.Session.GetString("nonce");
+            }
+        }
+
+        /// <summary>State</summary>
+        public string State
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(HttpContext.Session.GetString("state")))
+                {
+                    HttpContext.Session.SetString("state", GetPassword.Base64UrlSecret(10));
+                }
+                return HttpContext.Session.GetString("state");
+            }
+        }
 
         /// <summary>
         /// GET: Home
@@ -80,52 +109,64 @@ namespace MVC_Sample.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
-            // 通常ログイン
-            if (ModelState.IsValid)
+            if (!Request.Form.Keys.Any(x => x == "external"))
             {
-                if (!string.IsNullOrEmpty(model.UserName))
+                // 通常ログイン
+                if (ModelState.IsValid)
                 {
-                    // 認証情報を作成する。
-                    List<Claim> claims = new List<Claim>();
-                    claims.Add(new Claim(ClaimTypes.Name, model.UserName));
+                    if (!string.IsNullOrEmpty(model.UserName))
+                    {
+                        // 認証情報を作成する。
+                        List<Claim> claims = new List<Claim>();
+                        claims.Add(new Claim(ClaimTypes.Name, model.UserName));
 
-                    // 認証情報を保存する。
-                    ClaimsIdentity userIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                    ClaimsPrincipal userPrincipal = new ClaimsPrincipal(userIdentity);
+                        // 認証情報を保存する。
+                        ClaimsIdentity userIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                        ClaimsPrincipal userPrincipal = new ClaimsPrincipal(userIdentity);
 
-                    // サイン アップする。
-                    await AuthenticationHttpContextExtensions.SignInAsync(
-                        this.HttpContext, CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+                        // サイン アップする。
+                        await AuthenticationHttpContextExtensions.SignInAsync(
+                            this.HttpContext, CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
 
-                    // 認証情報を保存する。
-                    MyUserInfo ui = new MyUserInfo(model.UserName, (new GetClientIpAddress()).GetAddress());
-                    UserInfoHandle.SetUserInformation(ui);
+                        // 認証情報を保存する。
+                        MyUserInfo ui = new MyUserInfo(model.UserName, (new GetClientIpAddress()).GetAddress());
+                        UserInfoHandle.SetUserInformation(ui);
 
-                    //基盤に任せるのでリダイレクトしない。
-                    return View(model);
+                        //基盤に任せるのでリダイレクトしない。
+                        return View(model);
 
+                    }
+                    else
+                    {
+                        // ユーザー認証 失敗
+                        this.ModelState.AddModelError(string.Empty, "指定されたユーザー名またはパスワードが正しくありません。");
+                    }
                 }
                 else
                 {
-                    // ユーザー認証 失敗
-                    this.ModelState.AddModelError(string.Empty, "指定されたユーザー名またはパスワードが正しくありません。");
+                    // LoginViewModelの検証に失敗
                 }
+
+                // Session消去
+                //this.FxSessionAbandon();
+
+                // ポストバック的な
+                return this.View(model);
             }
             else
             {
-                // LoginViewModelの検証に失敗
+                // 外部ログイン
+                return Redirect(string.Format(
+                    "http://localhost:63359/MultiPurposeAuthSite/Account/OAuth2Authorize"
+                    + "?client_id=" + OAuth2AndOIDCParams.ClientID
+                    + "&response_type=code"
+                    + "&scope=profile%20email%20phone%20address%20openid"
+                    + "&state={0}"
+                    + "&nonce={1}"
+                    + "&prompt=none",
+                    this.State, this.Nonce));
             }
-
-            // Session消去
-            //this.FxSessionAbandon();
-
-            // ポストバック的な
-            return this.View(model);
         }
-
-        #endregion
-
-        #region 認証必要
 
         /// <summary>
         /// Get: /Home/Scroll
@@ -151,6 +192,85 @@ namespace MVC_Sample.Controllers
             return this.Redirect(Url.Action("Index", "Home"));
         }
 
-        #endregion
+        /// <summary>OAuth2AuthorizationCodeGrantClient</summary>
+        /// <param name="code">string</param>
+        /// <param name="state">string</param>
+        /// <returns>ActionResultを非同期的に返す</returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult> OAuth2AuthorizationCodeGrantClient(string code, string state)
+        {
+            try
+            {
+                string response = "";
+
+                if (state == this.State) // CSRF(XSRF)対策のstateの検証は重要
+                {
+                    response = await OAuth2AndOIDCClient.GetAccessTokenByCodeAsync(
+                        new Uri("http://localhost:63359/MultiPurposeAuthSite/OAuth2BearerToken"),
+                        OAuth2AndOIDCParams.ClientID, OAuth2AndOIDCParams.ClientSecret,
+                        HttpUtility.HtmlEncode("http://localhost:58496/Home/OAuth2AuthorizationCodeGrantClient"), code);
+
+                    // 汎用認証サイトはOIDCをサポートしたのでid_tokenを取得し、検証可能。
+                    //Base64UrlTextEncoder base64UrlEncoder = new Base64UrlTextEncoder();
+                    Dictionary<string, string> dic = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+
+                    // id_tokenの検証コード
+                    if (dic.ContainsKey("id_token"))
+                    {
+                        string id_token = dic["id_token"];
+
+                        string out_sub = "";
+                        string out_nonce = "";
+                        JObject jobj = null;
+
+                        if (IdToken.Verify(id_token, dic["access_token"], code, state, out out_sub, out out_nonce, out jobj)
+                            && out_nonce == this.Nonce)
+                        {
+                            // ログインに成功
+
+                            // /userinfoエンドポイントにアクセスする場合
+                            response = await OAuth2AndOIDCClient.GetUserInfoAsync(
+                                new Uri("http://localhost:63359/MultiPurposeAuthSite/userinfo"), dic["access_token"]);
+
+                            // 認証情報を作成する。
+                            List<Claim> claims = new List<Claim>();
+                            claims.Add(new Claim(ClaimTypes.Name, out_sub));
+
+                            // 認証情報を保存する。
+                            ClaimsIdentity userIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                            ClaimsPrincipal userPrincipal = new ClaimsPrincipal(userIdentity);
+
+                            // サイン アップする。
+                            await AuthenticationHttpContextExtensions.SignInAsync(
+                                this.HttpContext, CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+
+                            // 認証情報を保存する。
+                            MyUserInfo ui = new MyUserInfo(out_sub, (new GetClientIpAddress()).GetAddress());
+                            UserInfoHandle.SetUserInformation(ui);
+
+                            return this.Redirect(Url.Action("Index", "Home"));
+                        }
+
+                    }
+                    else { }
+                }
+                else { }
+
+                // ログインに失敗
+                return RedirectToAction("Login");
+            }
+            finally
+            {
+                this.ClearExLoginsParams();
+            }
+        }
+
+        /// <summary>ClearExLoginsParam</summary>
+        private void ClearExLoginsParams()
+        {
+            HttpContext.Session.SetString("nonce", "");
+            HttpContext.Session.SetString("state", "");
+        }
     }
 }
