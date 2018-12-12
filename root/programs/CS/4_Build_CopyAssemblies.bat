26905a3e49f52f9de455d277f5f868f3355ce518
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-12-12 23:36:38 +0900

    Now, I am corresponding to #322.

---
+++ root/programs/CS/4_Build_CopyAssemblies.bat
@@ -0,0 +1,17 @@
+@rem --------------------------------------------------
+@rem Turn off the echo function.
+@rem --------------------------------------------------
+@echo off
+
+@rem --------------------------------------------------
+@rem Copy for build local repository.
+@rem --------------------------------------------------
+xcopy /E /Y "Frameworks\Infrastructure\Build_net46" "Frameworks\Infrastructure\Build\"
+
+@rem --------------------------------------------------
+@rem Copy for build other repositories.
+@rem --------------------------------------------------
+xcopy /E /Y "Frameworks\Infrastructure\Build_net45" "C:\OpenTouryoAssemblies\Build_net45\"
+xcopy /E /Y "Frameworks\Infrastructure\Build_net46" "C:\OpenTouryoAssemblies\Build_net46\"
+xcopy /E /Y "Frameworks\Infrastructure\Build_net47" "C:\OpenTouryoAssemblies\Build_net47\"
+xcopy /E /Y "Frameworks\Infrastructure\Build_netcore20" "C:\OpenTouryoAssemblies\Build_netcore20\"
