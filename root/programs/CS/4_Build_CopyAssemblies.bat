3b0ce2f2486ecae89914c3c342cb21c61b914611
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-12-13 15:02:23 +0900

    Now, I am corresponding to #322.

--- root/programs/CS/4_Build_CopyAssemblies.bat
+++ root/programs/CS/4_Build_CopyAssemblies.bat
@@ -7,12 +7,4 @@
 @rem Copy for build local repository.
 @rem --------------------------------------------------
 xcopy /E /Y "Frameworks\Infrastructure\Build_net46" "Frameworks\Infrastructure\Build\"
-
-@rem --------------------------------------------------
-@rem Copy for build other repositories.
-@rem --------------------------------------------------
-xcopy /E /Y "Frameworks\Infrastructure\Build_net45" "C:\OpenTouryoAssemblies\Build_net45\"
-xcopy /E /Y "Frameworks\Infrastructure\Build_net46" "C:\OpenTouryoAssemblies\Build_net46\"
-xcopy /E /Y "Frameworks\Infrastructure\Build_net47" "C:\OpenTouryoAssemblies\Build_net47\"
-xcopy /E /Y "Frameworks\Infrastructure\Build_netstandard20" "C:\OpenTouryoAssemblies\Build_netstandard20\"
-xcopy /E /Y "Frameworks\Infrastructure\Build_netcore20" "C:\OpenTouryoAssemblies\Build_netcore20\"
+xcopy /E /Y "Frameworks\Infrastructure\Build_netcore20" "C:\OpenTouryoAssemblies\Build_netcore\"
---
+++ root/programs/CS/99_BuildTheseInOtherRepositoriesWithDev.bat
@@ -0,0 +1,55 @@
+﻿@echo off
+
+@rem 【Bat】【vim】香り屋Vimをダウンロードしてインストールまでするbatファイル - Qiita
+@rem https://qiita.com/koryuohproject/items/beed1a28ad6a1f60256d
+
+setlocal
+
+@rem ZIPファイル名
+set zipfilename=Temp.zip
+
+@rem GitHubパス
+set srcUrl=https://github.com/OpenTouryoProject/OpenTouryo/archive/develop.zip
+
+@rem 解凍ディレクトリ
+set extDir=%CD%
+
+@rem 一時ディレクトリ
+set tmpDir=Temp
+
+:Download
+@rem ダウンロードされたデータがあるなら展開へ
+if exist %extDir%\%zipfilename% GOTO Extract
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "$d=new-object System.Net.WebClient; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; $d.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetWorkCredentials; $d.DownloadFile('%srcUrl%','%extDir%/%zipfilename%')"
+
+:Extract
+@rem 一時ディレクトリがあるならビルドへ
+if exist %extDir%\%tmpDir% GOTO Build
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "expand-archive %zipfilename%"
+
+:Build
+@rem ビルドがあるならコピーへ
+if exist "Temp\OpenTouryo-develop\root\programs\CS\Frameworks\Infrastructure\Build_netcore20" GOTO Xcopy
+
+cd "Temp\OpenTouryo-develop\root\programs\CS\"
+
+call 2_Build_NuGet_net45.bat
+call 2_Build_NuGet_net46.bat
+call 2_Build_NuGet_net47.bat
+call 2_Build_NuGet_netstd20.bat
+call 3_Build_Business_net45.bat
+call 3_Build_Business_net46.bat
+call 3_Build_Business_net47.bat
+call 3_Build_Business_netcore20.bat
+
+:Xcopy
+cd extDir
+xcopy /E /Y "Temp\OpenTouryo-develop\root\programs\CS\Frameworks\Infrastructure\Build_net45" "OpenTouryoAssemblies\Build_net45\"
+xcopy /E /Y "Temp\OpenTouryo-develop\root\programs\CS\Frameworks\Infrastructure\Build_net46" "OpenTouryoAssemblies\Build_net46\"
+xcopy /E /Y "Temp\OpenTouryo-develop\root\programs\CS\Frameworks\Infrastructure\Build_net47" "OpenTouryoAssemblies\Build_net47\"
+xcopy /E /Y "Temp\OpenTouryo-develop\root\programs\CS\Frameworks\Infrastructure\Build_netcore20" "OpenTouryoAssemblies\Build_netcore20\"
+
+:EOF
+endlocal
