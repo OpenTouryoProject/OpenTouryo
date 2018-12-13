bb3b5e38c2ff7b25b1d00c4492c18443f4d88a24
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-12-13 15:34:50 +0900

    Now, I am corresponding to #322.

--- root/programs/VB/z_ChangePackages_net45.bat
+++ root/programs/VB/z_ChangePackages_net45.bat
@@ -1,7 +1,1 @@
-copy /Y "Frameworks\Infrastructure\Public\packages_net45.config" "Frameworks\Infrastructure\Public\packages.config"
-copy /Y "Frameworks\Infrastructure\Public\Db\DamManagedOdp\packages_net45.config" "Frameworks\Infrastructure\Public\Db\DamManagedOdp\packages.config"
-copy /Y "Frameworks\Infrastructure\Public\Db\DamPstGrS\packages_net45.config" "Frameworks\Infrastructure\Public\Db\DamPstGrS\packages.config"
-copy /Y "Frameworks\Infrastructure\Public\Db\DamMySQL\packages_net45.config" "Frameworks\Infrastructure\Public\Db\DamMySQL\packages.config"
-
-copy /Y "Frameworks\Infrastructure\Framework\packages_net45.config" "Frameworks\Infrastructure\Framework\packages.config"
 copy /Y "Frameworks\Infrastructure\Business\packages_net45.config" "Frameworks\Infrastructure\Business\packages.config"
