ea8524f7b7e4ac792740a47d8cdb63acebf44cc0
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-12-13 18:08:30 +0900

    Now, I am corresponding to #322.

---
+++ root/programs/VB/1_GetLibrariesFromCS.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem --------------------------------------------------
+rem Get libraries from C# folder.
+rem --------------------------------------------------
+xcopy /E /Y "..\CS\Frameworks\Infrastructure\Build_net45" "Frameworks\Infrastructure\Build_net45\"
+xcopy /E /Y "..\CS\Frameworks\Infrastructure\Build_net46" "Frameworks\Infrastructure\Build_net46\"
+xcopy /E /Y "..\CS\Frameworks\Infrastructure\Build_net47" "Frameworks\Infrastructure\Build_net47\"
+
+pause
--- root/programs/VB/3_Build_Business_net45.bat
+++ root/programs/VB/3_Build_Business_net45.bat
@@ -21,12 +21,6 @@ rem --------------------------------------------------
 call %CURRENT_DIR%z_ChangePackages_net45.bat
 
 rem --------------------------------------------------
-rem Get libraries from C# folder.
-rem --------------------------------------------------
-xcopy /E /Y "..\CSFrameworks\Infrastructure\Build_net45" "Frameworks\Infrastructure\Build_net45\"
-del "Frameworks\Infrastructure\Build_net45\" /f /s /a- *Business*
-
-rem --------------------------------------------------
 rem Build the Infrastructures
 rem --------------------------------------------------
 
