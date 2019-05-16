cbc2a11b507b96b0534b8405cf1bf453040b487d
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2019-05-16 10:43:37 +0900

    Additional commit for #332.

--- root/programs/CS/Samples/WebApp_sample/WebForms_Sample/WebForms_Sample/AppCode/sample/Business/LayerB.cs
+++ root/programs/CS/Samples/WebApp_sample/WebForms_Sample/WebForms_Sample/AppCode/sample/Business/LayerB.cs
@@ -530,6 +530,9 @@ namespace WebForms_Sample
 
                 case "generate": // 自動生成Daoを使用する。
 
+                    // 自動生成Dao性能対策 - Open 棟梁 Wiki
+                    // https://opentouryo.osscons.jp/index.php?%E8%87%AA%E5%8B%95%E7%94%9F%E6%88%90Dao%E6%80%A7%E8%83%BD%E5%AF%BE%E7%AD%96
+
                     // 自動生成Daoを生成
                     // https://www.guidgenerator.com/online-guid-generator.aspx
                     DaoShippers genDao = new DaoShippers(this.GetDam(), "f54d4d7bd5c8441187ec6939c4da7303");
