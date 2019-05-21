171aec4d04c7312eeb3f7a7a6306046441d1bb9a
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2019-05-21 11:45:51 +0900

    Now, I am corresponding to #338.

---
+++ root/programs/CS/Frameworks/Infrastructure/Framework/Authentication/SAML2Bindings.cs
@@ -0,0 +1,51 @@
+﻿//**********************************************************************************
+//* Copyright (C) 2017 Hitachi Solutions,Ltd.
+//**********************************************************************************
+
+#region Apache License
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+//**********************************************************************************
+//* クラス名        ：SAML2Bindings
+//* クラス日本語名  ：SAML2Bindings（ライブラリ）
+//*
+//* 作成日時        ：－
+//* 作成者          ：－
+//* 更新履歴        ：－
+//*
+//*  日時        更新者            内容
+//*  ----------  ----------------  -------------------------------------------------
+//*  2019/05/21  西野 大介         新規作成
+//**********************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using System.Web;
+using System.Net.Http;
+
+using Touryo.Infrastructure.Public.Str;
+using Touryo.Infrastructure.Public.Security;
+
+namespace Touryo.Infrastructure.Framework.Authentication
+{
+    /// <summary>SAML2Bindings（ライブラリ）</summary>
+    public class SAML2Bindings
+    {
+    }
+}
