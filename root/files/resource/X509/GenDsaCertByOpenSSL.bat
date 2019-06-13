2b30151ef4059366564a875cf07a90b75e1c0989
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2019-06-13 09:42:21 +0900

    Now, I am corresponding to #338.

--- root/files/resource/X509/GenDsaCertByOpenSSL.bat
+++ root/files/resource/X509/GenDsaCertByOpenSSL.bat
@@ -2,5 +2,4 @@ openssl dsaparam -out dsaparam.out 2048
 openssl gendsa -out private-key.pem dsaparam.out
 openssl req -new -key private-key.pem > csr.csr
 openssl x509 -in csr.csr -days 365000 -req -signkey private-key.pem > _SHA256DSA.cer
-openssl pkcs12 -export -out _SHA256DSA.pfx -inkey private-key.pem -in _SHA256DSA.cer
-:openssl pkcs12 -in _SHA256DSA.pfx -out _SHA256DSA.cer -nokeys -clcerts
+openssl pkcs12 -export -inkey private-key.pem -in _SHA256DSA.cer > _SHA256DSA.pfx
