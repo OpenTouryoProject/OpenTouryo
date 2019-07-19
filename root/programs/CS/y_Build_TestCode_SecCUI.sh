c34234a7d581e1b86375d918e860a5ca0b90265d
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2019-07-19 17:54:35 +0900

    Added the first *.sh file.

---
+++ root/programs/CS/y_Build_TestCode_SecCUI.sh
@@ -0,0 +1,14 @@
+# 以下の様にしてWSLから実行する。改行コードはLFに変更してある。
+#$ cd /mnt/c/Git1/OpenTouryo/root/programs/CS
+#$ ./y_Build_TestCode_SecCUI.sh
+
+
+cd /mnt/c/Git1/OpenTouryo/root/programs/CS/Frameworks/Tests/EncAndDecUtilCUI/core20
+dotnet publish -c Release -r ubuntu.16.04-x64 --self-contained
+cd bin/Release/netcoreapp2.0/ubuntu.16.04-x64
+dotnet EncAndDecUtilCUICore.dll > ../../../../../ResultCore20OnLinux.txt
+
+cd /mnt/c/Git1/OpenTouryo/root/programs/CS/Frameworks/Tests/EncAndDecUtilCUI/core30
+dotnet publish -c Release -r ubuntu.16.04-x64 --self-contained
+cd bin/Release/netcoreapp3.0/ubuntu.16.04-x64
+dotnet EncAndDecUtilCUICore.dll > ../../../../../ResultCore30OnLinux.txt
