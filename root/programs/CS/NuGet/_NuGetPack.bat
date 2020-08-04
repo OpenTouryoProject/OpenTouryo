456689df706a04cf02dfc10e271dc5ae097a2072
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2020-08-04 23:43:00 +0900

    Started preview, because interface changes have occurred.

--- root/programs/CS/NuGet/_NuGetPack.bat
+++ root/programs/CS/NuGet/_NuGetPack.bat
@@ -7,6 +7,7 @@ xcopy /E /Y "..\Frameworks\Infrastructure\Build_net47" "in\net47"
 xcopy /E /Y "..\Frameworks\Infrastructure\Build_net48" "in\net48"
 xcopy /E /Y "..\Frameworks\Infrastructure\Build_netstd20\netstandard2.0" "in\netstandard2.0"
 xcopy /E /Y "..\Frameworks\Infrastructure\Build_netstd21\netstandard2.1" "in\netstandard2.1"
+xcopy /E /Y "..\Frameworks\Infrastructure\Build_netcore30\netcoreapp3.0" "in\netcoreapp3.0"
 
 "..\..\nuget.exe" pack Public.nuspec -OutputDirectory "out\pp"
 "..\..\nuget.exe" pack Public.Security.nuspec -OutputDirectory "out\pp"
@@ -16,12 +17,12 @@ xcopy /E /Y "..\Frameworks\Infrastructure\Build_netstd21\netstandard2.1" "in\net
 "..\..\nuget.exe" pack DamPstGrS.nuspec -OutputDirectory "out\pp"
 "..\..\nuget.exe" pack DamMySQL.nuspec -OutputDirectory "out\pp"
 
-"..\..\nuget.exe" pack Symbol_Public.nuspec -OutputDirectory "out\sp" -Symbols
-"..\..\nuget.exe" pack Symbol_Public.Security.nuspec -OutputDirectory "out\sp" -Symbols
-"..\..\nuget.exe" pack Symbol_Framework.nuspec -OutputDirectory "out\sp" -Symbols
-"..\..\nuget.exe" pack Symbol_Framework.RichClient.nuspec -OutputDirectory "out\sp" -Symbols
-"..\..\nuget.exe" pack Symbol_DamManagedOdp.nuspec -OutputDirectory "out\sp" -Symbols
-"..\..\nuget.exe" pack Symbol_DamPstGrS.nuspec -OutputDirectory "out\sp" -Symbols
-"..\..\nuget.exe" pack Symbol_DamMySQL.nuspec -OutputDirectory "out\sp" -Symbols
+"..\..\nuget.exe" pack Symbol_Public.nuspec -OutputDirectory "out\sp" -Symbols -SymbolPackageFormat snupkg
+"..\..\nuget.exe" pack Symbol_Public.Security.nuspec -OutputDirectory "out\sp" -Symbols -SymbolPackageFormat snupkg
+"..\..\nuget.exe" pack Symbol_Framework.nuspec -OutputDirectory "out\sp" -Symbols -SymbolPackageFormat snupkg
+"..\..\nuget.exe" pack Symbol_Framework.RichClient.nuspec -OutputDirectory "out\sp" -Symbols -SymbolPackageFormat snupkg
+"..\..\nuget.exe" pack Symbol_DamManagedOdp.nuspec -OutputDirectory "out\sp" -Symbols -SymbolPackageFormat snupkg
+"..\..\nuget.exe" pack Symbol_DamPstGrS.nuspec -OutputDirectory "out\sp" -Symbols -SymbolPackageFormat snupkg
+"..\..\nuget.exe" pack Symbol_DamMySQL.nuspec -OutputDirectory "out\sp" -Symbols -SymbolPackageFormat snupkg
 
 pause
--- root/programs/CS/NuGet/out/sp/_NuGetPush.bat
+++ root/programs/CS/NuGet/out/sp/_NuGetPush.bat
@@ -3,7 +3,7 @@
 
 "..\..\..\..\nuget.exe" SetApiKey [ApiKey]
 
-"..\..\..\..\nuget.exe" push Touryo.Infrastructure.Public.*.symbols.nupkg -source https://nuget.smbsrc.net/
-"..\..\..\..\nuget.exe" push Touryo.Infrastructure.Framework.*.symbols.nupkg -source https://nuget.smbsrc.net/
+"..\..\..\..\nuget.exe" push Touryo.Infrastructure.Public.*.nupkg -source https://api.nuget.org/v3/index.json
+"..\..\..\..\nuget.exe" push Touryo.Infrastructure.Framework.*.nupkg -source https://api.nuget.org/v3/index.json
 
 pause
