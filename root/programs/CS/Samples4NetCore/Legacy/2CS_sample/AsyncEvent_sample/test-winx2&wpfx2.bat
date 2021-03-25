0828d32bd4066d6e07454bab4d094ec251eb4ef4
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2021-03-25 19:24:35 +0900

    fixed #362

--- root/programs/CS/Samples4NetCore/Legacy/2CS_sample/AsyncEvent_sample/test-winx2&wpfx2.bat
+++ root/programs/CS/Samples4NetCore/Legacy/2CS_sample/AsyncEvent_sample/test-winx2&wpfx2.bat
@@ -1,5 +1,5 @@
-start WindowsFormsApplication\bin\Debug\netcoreapp3.0\WindowsFormsApplication.exe /WinForm1,WinForm2,WPF1
-start WindowsFormsApplication\bin\Debug\netcoreapp3.0\WindowsFormsApplication.exe /WinForm2,WinForm1,WPF2
+start WindowsFormsApplication\bin\Debug\net5.0\WindowsFormsApplication.exe /WinForm1,WinForm2,WPF1
+start WindowsFormsApplication\bin\Debug\net5.0\WindowsFormsApplication.exe /WinForm2,WinForm1,WPF2
 
-start WpfApplication\bin\Debug\netcoreapp3.0\WpfApplication.exe /WPF1,WPF2,WinForm1
-start WpfApplication\bin\Debug\netcoreapp3.0\WpfApplication.exe /WPF2,WPF1,WinForm2
+start WpfApplication\bin\Debug\net5.0\WpfApplication.exe /WPF1,WPF2,WinForm1
+start WpfApplication\bin\Debug\net5.0\WpfApplication.exe /WPF2,WPF1,WinForm2
