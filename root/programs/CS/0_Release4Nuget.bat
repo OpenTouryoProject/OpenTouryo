4c230f3369771dab79b195da706920ac1250cece
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2021-03-29 16:53:50 +0900

    Tweak.

--- root/programs/CS/0_Release4Nuget.bat
+++ root/programs/CS/0_Release4Nuget.bat
@@ -11,21 +11,7 @@ echo | call 2_Build_NuGet_net48.bat
 echo | call 2_Build_NuGet_netstd20.bat
 echo | call 2_Build_NuGet_netstd21.bat
 echo | call 2_Build_NuGet_nettcore30.bat
-
-@echo on
-timeout 5
-
-echo | call 3_Build_Business_net45.bat
-echo | call 3_Build_Business_net46.bat
-echo | call 3_Build_Business_net47.bat
-echo | call 3_Build_Business_net48.bat
-echo | call 3_Build_Business_netcore20.bat
-echo | call 3_Build_Business_netcore30.bat
-echo | call 3_Build_BusinessRichClient_net45.bat
-echo | call 3_Build_BusinessRichClient_net46.bat
-echo | call 3_Build_BusinessRichClient_net47.bat
-echo | call 3_Build_BusinessRichClient_net48.bat
-echo | call 3_Build_BusinessRichClient_netcore30.bat
+echo | call 2_Build_NuGet_nettcore50.bat
 
 echo | call 4_Build_CopyAssemblies.bat
 
--- root/programs/CS/NuGet/_NuGetPack.bat
+++ root/programs/CS/NuGet/_NuGetPack.bat
@@ -8,6 +8,8 @@ xcopy /E /Y "..\Frameworks\Infrastructure\Build_net48" "in\net48"
 xcopy /E /Y "..\Frameworks\Infrastructure\Build_netstd20\netstandard2.0" "in\netstandard2.0"
 xcopy /E /Y "..\Frameworks\Infrastructure\Build_netstd21\netstandard2.1" "in\netstandard2.1"
 xcopy /E /Y "..\Frameworks\Infrastructure\Build_netcore30\netcoreapp3.0" "in\netcoreapp3.0"
+xcopy /E /Y "..\Frameworks\Infrastructure\Build_netcore50\net5.0" "in\net5.0"
+xcopy /E /Y "..\Frameworks\Infrastructure\Build_netcore50\net5.0-windows" "in\net5.0-windows"
 
 "..\..\nuget.exe" pack Symbol_Public.nuspec -OutputDirectory "out\sp" -Symbols -SymbolPackageFormat snupkg
 "..\..\nuget.exe" pack Symbol_Public.Security.nuspec -OutputDirectory "out\sp" -Symbols -SymbolPackageFormat snupkg
