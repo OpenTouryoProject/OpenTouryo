99488e2968709ce14a795d21e654bca5899f4a55
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2021-05-20 15:59:20 +0900

    Now, I am fixing #398.

--- root/programs/CS/Samples/CLI_sample/Simple_CLI/Simple_CLI/Program.cs
+++ root/programs/CS/Samples/CLI_sample/Simple_CLI/Simple_CLI/Program.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿//**********************************************************************************
+//* 単純CLIサンプル アプリ
+//**********************************************************************************
+
+// テスト用サンプルなので、必要に応じて流用 or 削除して下さい。
+
+//**********************************************************************************
+//* クラス名        ：Program
+//* クラス日本語名  ：単純CLIサンプル アプリ
+//*
+//* 作成日時        ：－
+//* 作成者          ：開発基盤部会
+//* 更新履歴        ：
+//*
+//*  日時        更新者            内容
+//*  ----------  ----------------  -------------------------------------------------
+//*  20xx/xx/xx  ＸＸ ＸＸ         ＸＸＸＸ
+//**********************************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +27,8 @@ using System.IO;
 using System.Threading;
 using System.Diagnostics;
 
+using Sharprompt;
+
 using System.CommandLine;
 using System.CommandLine.Invocation;
 
@@ -16,7 +37,7 @@ using Newtonsoft.Json;
 
 namespace Simple_CLI
 {
-	/// <summary>Program</summary>
+    /// <summary>Program</summary>
     public class Program
     {
         /// <summary>
@@ -92,6 +113,36 @@ namespace Simple_CLI
             rootCommand.AddCommand(subCommandComplex);
             #endregion
 
+            #region subCommandInteractive
+            Command subCommandInteractive = new Command(name: "interactive", description: "Sub command interactive");
+            subCommandInteractive.Handler = CommandHandler.Create(() =>
+            {
+                Prompt.ColorSchema.Answer = ConsoleColor.DarkRed;
+                Prompt.ColorSchema.Select = ConsoleColor.DarkCyan;
+                Console.OutputEncoding = Encoding.UTF8;
+
+                string name = Prompt.Input<string>("名前", validators: new[] { Validators.Required() });
+                Console.WriteLine($"こんにちは, {name}");
+
+                int age = Prompt.Input<int>("年齢");
+                Console.WriteLine($"年齢: {age}");
+
+                string password = Prompt.Password("Type new password");
+                Console.WriteLine("Password OK");
+
+                bool answer = Prompt.Confirm("Are you ready?");
+                Console.WriteLine($"Your answer is {answer}");
+
+                EnumMonth value = Prompt.Select<EnumMonth>("Select enum value");
+                Console.WriteLine($"You selected {value}");
+
+                Program.GetPrefectureList(out string[] prefectureList);
+                string prefecture = Prompt.Select("都道府県", prefectureList, pageSize: 5);
+                Console.WriteLine($"都道府県: {prefecture}");
+            });
+            rootCommand.AddCommand(subCommandInteractive);
+            #endregion
+
             #region subCommandDelay
             Command subCommandDelay = new Command(name: "delay", description: "Sub command delay");
             subCommandDelay.AddOption(new Option<string>(alias: "--a-string"));
@@ -108,7 +159,7 @@ namespace Simple_CLI
 
                             await Task.Delay(1000, token);
                         }
-                        
+
                         Console.WriteLine("Sub command delay was completed.");
                         return 0;
                     }
@@ -185,9 +236,78 @@ namespace Simple_CLI
             await rootCommand.InvokeAsync("cmd2 --a-string hoge");
             // subCommandComplex
             await rootCommand.InvokeAsync("complex --an-int 123 --a-string hogehoge");
+            // subCommandInteractive
+            await rootCommand.InvokeAsync("interactive");
             // subCommandDelay
             await rootCommand.InvokeAsync("delay --a-string hogehoge");
             #endregion
+        }
+        #endregion
+
+        #region SELECT
+
+        /// <summary>EnumMonth</summary>
+        private enum EnumMonth : byte
+        {
+            January = 1, February, March, April,
+            May, June, July, August,
+            September, October, November, December
+        }
+
+        /// <summary>GetPrefectureList</summary>
+        /// <param name="prefectureList">string[]</param>
+        private static void GetPrefectureList(out string[] prefectureList)
+        {
+            prefectureList = new[]
+            {
+              "北海道",
+              "青森県",
+              "岩手県",
+              "宮城県",
+              "秋田県",
+              "山形県",
+              "福島県",
+              "茨城県",
+              "栃木県",
+              "群馬県",
+              "埼玉県",
+              "千葉県",
+              "東京都",
+              "神奈川県",
+              "新潟県",
+              "富山県",
+              "石川県",
+              "福井県",
+              "山梨県",
+              "長野県",
+              "岐阜県",
+              "静岡県",
+              "愛知県",
+              "三重県",
+              "滋賀県",
+              "京都府",
+              "大阪府",
+              "兵庫県",
+              "奈良県",
+              "和歌山県",
+              "鳥取県",
+              "島根県",
+              "岡山県",
+              "広島県",
+              "山口県",
+              "徳島県",
+              "香川県",
+              "愛媛県",
+              "高知県",
+              "福岡県",
+              "佐賀県",
+              "長崎県",
+              "熊本県",
+              "大分県",
+              "宮崎県",
+              "鹿児島県",
+              "沖縄県"
+            };
         }
         #endregion
     }
