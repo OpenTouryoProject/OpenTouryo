1dde59c4ee6cfaf131e275455267ded9985e455a
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2021-05-23 15:12:31 +0900

    Now, I am fixing #398.

--- root/programs/CS/Samples4NetCore/Legacy/CLI_sample/DAG_Login_CLI/DAG_Login_CLI/Program.cs
+++ root/programs/CS/Samples4NetCore/Legacy/CLI_sample/DAG_Login_CLI/DAG_Login_CLI/Program.cs
@@ -101,88 +101,11 @@ namespace DAG_Login_CLI
             rootCommand.AddCommand(subCommand2);
             #endregion
 
-            #region subCommandComplex
-            Command subCommandComplex = new Command(name: "complex", description: "Sub command complex")
-            {
-                new Option<int>("--an-int"),
-                new Option<string>("--a-string")
-            };
-            subCommandComplex.Handler = CommandHandler.Create((ComplexType complexType) =>
-            {
-                Console.WriteLine($"Sub command complex: {JsonConvert.SerializeObject(complexType)}");
-            });
-            rootCommand.AddCommand(subCommandComplex);
-            #endregion
-
-            #region subCommandInteractive
-            Command subCommandInteractive = new Command(name: "interactive", description: "Sub command interactive");
-            subCommandInteractive.AddOption(new Option<string>(alias: "--a-string"));
-            subCommandInteractive.Handler = CommandHandler.Create((string aString) =>
-            {
-                Console.WriteLine($"Sub command interactive (Ctrl-C terminate): {aString}");
-                
-                Prompt.ColorSchema.Answer = ConsoleColor.DarkRed;
-                Prompt.ColorSchema.Select = ConsoleColor.DarkCyan;
-                Console.OutputEncoding = Encoding.UTF8;
-
-                string name = Prompt.Input<string>("名前", validators: new[] { Validators.Required() });
-                Console.WriteLine($"こんにちは, {name}");
-
-                int age = Prompt.Input<int>("年齢");
-                Console.WriteLine($"年齢: {age}");
-
-                string password = Prompt.Password("Type new password");
-                Console.WriteLine("Password OK");
-
-                bool answer = Prompt.Confirm("Are you ready?");
-                Console.WriteLine($"Your answer is {answer}");
-
-                EnumMonth value = Prompt.Select<EnumMonth>("Select enum value");
-                Console.WriteLine($"You selected {value}");
-
-                Program.GetPrefectureList(out string[] prefectureList);
-                string prefecture = Prompt.Select("都道府県", prefectureList, pageSize: 5);
-                Console.WriteLine($"都道府県: {prefecture}");
-            });
-            rootCommand.AddCommand(subCommandInteractive);
-            #endregion
-
-            #region subCommandDelay
-            Command subCommandDelay = new Command(name: "delay", description: "Sub command delay");
-            subCommandDelay.AddOption(new Option<string>(alias: "--a-string"));
-            subCommandDelay.Handler = CommandHandler.Create<string, IConsole, CancellationToken>(
-                async (string aString, IConsole console, CancellationToken token) =>
-                {
-                    Console.WriteLine($"Sub command delay(Ctrl-C terminate): {aString}");
-
-                    try
-                    {
-                        for (int i = 0; i < 10; i++)
-                        {
-                            Console.WriteLine($"Sub command delay: {i}");
-
-                            await Task.Delay(1000, token);
-                        }
-
-                        // 例えば...
-                        //using (var httpClient = new HttpClient())
-                        //{
-                        //    await httpClient.GetAsync("http://www.example.com", token);
-                        //}
-
-                        Console.WriteLine("Sub command delay was completed.");
-                        return 0;
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        // ココのコードは.NET Fx系だとハングする。
-                        // .NET Core系だとハングしない。
-                        Console.WriteLine("Sub command delay was aborted.");
-                        return 1;
-                    }
-                }
-            );
-            rootCommand.AddCommand(subCommandDelay);
+            #region LoginCommand
+            Command LoginCommand = new Command(name: "login", description: "Sub command login");
+            LoginCommand.AddOption(new Option<string>(alias: "--a-string"));
+            LoginCommand.Handler = CommandHandler.Create<string>(Program.LoginCommand);
+            rootCommand.AddCommand(LoginCommand);
             #endregion
 
             #endregion
@@ -195,6 +118,7 @@ namespace DAG_Login_CLI
         }
 
         #region Command
+
         #region RootCommand
         /// <summary>RootCommand</summary>
         /// <param name="intOption">int</param>
@@ -211,6 +135,16 @@ namespace DAG_Login_CLI
                 $"--file-access-option is: {fileAccessOption.ToString()}");
         }
         #endregion
+
+        #region LoginCommand
+        /// <summary>LoginCommand</summary>
+        /// <param name="aString">aString</param>
+        private static void LoginCommand(string aString)
+        {
+            Console.WriteLine($"Sub command login: {aString}");
+        }
+        #endregion
+
         #endregion
 
         #region TEST
@@ -246,93 +180,10 @@ namespace DAG_Login_CLI
             await rootCommand.InvokeAsync("cmd1 --an-int 123");
             // subCommand2
             await rootCommand.InvokeAsync("cmd2 --a-string hoge");
-            // subCommandComplex
-            await rootCommand.InvokeAsync("complex --an-int 123 --a-string hogehoge");
-            // subCommandInteractive
-            await rootCommand.InvokeAsync("interactive --a-string hoge");
-            // subCommandDelay
-            await rootCommand.InvokeAsync("delay --a-string hogehoge");
+            // subCommandLogin
+            await rootCommand.InvokeAsync("login");
             #endregion
         }
         #endregion
-
-        #region SELECT
-
-        /// <summary>EnumMonth</summary>
-        private enum EnumMonth : byte
-        {
-            January = 1, February, March, April,
-            May, June, July, August,
-            September, October, November, December
-        }
-
-        /// <summary>GetPrefectureList</summary>
-        /// <param name="prefectureList">string[]</param>
-        private static void GetPrefectureList(out string[] prefectureList)
-        {
-            prefectureList = new[]
-            {
-              "北海道",
-              "青森県",
-              "岩手県",
-              "宮城県",
-              "秋田県",
-              "山形県",
-              "福島県",
-              "茨城県",
-              "栃木県",
-              "群馬県",
-              "埼玉県",
-              "千葉県",
-              "東京都",
-              "神奈川県",
-              "新潟県",
-              "富山県",
-              "石川県",
-              "福井県",
-              "山梨県",
-              "長野県",
-              "岐阜県",
-              "静岡県",
-              "愛知県",
-              "三重県",
-              "滋賀県",
-              "京都府",
-              "大阪府",
-              "兵庫県",
-              "奈良県",
-              "和歌山県",
-              "鳥取県",
-              "島根県",
-              "岡山県",
-              "広島県",
-              "山口県",
-              "徳島県",
-              "香川県",
-              "愛媛県",
-              "高知県",
-              "福岡県",
-              "佐賀県",
-              "長崎県",
-              "熊本県",
-              "大分県",
-              "宮崎県",
-              "鹿児島県",
-              "沖縄県"
-            };
-        }
-        #endregion
-    }
-
-    /// <summary>ComplexType</summary>
-    public class ComplexType
-    {
-        // public ComplexType(int anInt, string aString)
-        // {
-        //     AnInt = anInt;
-        //     AString = aString;
-        // }
-        public int AnInt { get; set; }
-        public string AString { get; set; }
     }
 }
