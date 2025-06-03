5ad77ebaae8d05b5f008772153cf6ba294c817b0
nishi_74322014 <nishi_74322014@ksj.biglobe.ne.jp>
2025-06-04 02:19:16 +0900

    11th commit in migration branch.

--- root/programs/CS/0_ExecAllBat.bat
+++ root/programs/CS/0_ExecAllBat.bat
@@ -2,12 +2,14 @@
 timeout 5
 
 echo | call 1_DeleteDir.bat
+echo | call 1_DeleteFile.bat
 echo | call 2_Build_NuGet_netstd20.bat
 
 @echo on
 timeout 5
 
 echo | call 1_DeleteDir.bat
+echo | call 1_DeleteFile.bat
 echo | call 2_Build_NuGet_net48.bat
 echo | call 3_Build_Business_net48.bat
 echo | call 3_Build_BusinessRichClient_net48.bat
@@ -16,6 +18,7 @@ echo | call 3_Build_BusinessRichClient_net48.bat
 timeout 5
 
 echo | call 1_DeleteDir.bat
+echo | call 1_DeleteFile.bat
 echo | call 2_Build_NuGet_nettcore80.bat
 echo | call 3_Build_Business_netcore80.bat
 echo | call 3_Build_BusinessRichClient_netcore80.bat
@@ -29,6 +32,7 @@ echo | call 4_Build_CopyAssemblies.bat
 timeout 5
 
 echo | call 1_DeleteDir.bat
+echo | call 1_DeleteFile.bat
 echo | call 4_Build_Framework_Tool.bat
 echo | call 5_Build_2CS_sample.bat
 echo | call 5_Build_Bat_sample.bat
@@ -42,6 +46,7 @@ echo | call 10_Build_WebApp_sample.bat
 timeout 5
 
 echo | call 1_DeleteDir.bat
+echo | call 1_DeleteFile.bat
 echo | call 4_Build_Framework_ToolCore.bat
 echo | call 5_Build_2CSCore_sample.bat
 echo | call 5_Build_BatCore_sample.bat
--- root/programs/CS/1_DeleteDir.bat
+++ root/programs/CS/1_DeleteDir.bat
@@ -18,23 +18,3 @@ for %%a in ( %w2% ) do (
 )
 
 pause
-
-set DIRECTORIES=Build, Build_net45, Build_net46, Build_net47, Build_net48, Build_netstd20, Build_netstd21, Build_netcore20, Build_netcore30, Build_netcore50, Build_netcore60, Build_netcore80
-
-@echo --------------------------------------------------
-@echo Delete the %DIRECTORIES% folders.
-@echo --------------------------------------------------
-
-@rem カンマをスペースに変換
-set w1=%DIRECTORIES:,= %
-
-@rem 連続したスペースを、スペース１個に変換
-set w2=%w1:  = %
-
-for %%a in ( %w2% ) do (
-  for /D /R %%i in ( %%a ) do (
-    if exist "%%~i" RD /S /Q "%%~i"
-  )
-)
-
-pause
